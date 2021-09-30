e3846616c94c250e8867d34d7d6c326fec4c5004
benjamin-d6 <63401179+benjamin-d6@users.noreply.github.com>
2021-09-30 11:40:14 +0200

    sound

--- Assets/Scripts/deathtransition.cs
+++ Assets/Scripts/deathtransition.cs
@@ -6,11 +6,13 @@ public class deathtransition : MonoBehaviour
 {
     Animator m_Animator;
     bool death;
+    public AudioSource audioData;
     // Start is called before the first frame update
     void Start()
     {
         m_Animator = gameObject.GetComponent<Animator>();
         death = false;
+        audioData.Play(0);
     }
 
     // Update is called once per frame
@@ -19,6 +21,7 @@ public class deathtransition : MonoBehaviour
         if(death == true) { 
         m_Animator.Play("Scream");
         }
+      
     }
 
     private void OnTriggerEnter(Collider other)
